17e9d145b7914c85b3da0bacf16b2f27a302ffdd
Atharva Bhole <atharvabhole239@gmail.com>
2025-01-09 12:58:41 +0530

    Completed backend socketio functionality remaining

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.route.js";
+
 const app = express();
 
 dotenv.config();
@@ -12,7 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-
+app.use("api/message", messageRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port : ${process.env.PORT}`);
